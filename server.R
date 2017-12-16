0de13c996dce5232961e28f300873066338af2b7
Shuni <sli@macalester.edu>
2017-12-16 12:01:07 -0600

    split into sections add instructions

--- server.R
+++ server.R
@@ -4,20 +4,18 @@ library(devtools)
 library(coda)
 library(shiny)
 
+###############################################################################
 set.seed(2017)
+data = c(70, 80, 75, 83, 72)
+data = data.frame(data)
+data = sort(data[,1])
+################################################################################
+meandata <- mean(data)
+standarddeviationdata <- sd(data)
+mediandata <- median(data)
+rangedata <- max(data) - min(data)
 
-dat = c(70, 80, 75, 83, 72)
-dat = data.frame(dat)
-dat = sort(dat[,1])
-
-# Set up number of simulations and number of parameters to save
-reps  =  10000
-threshold = 30  #Tuning threshold in future
-prior_mean = 85
-prior_sd = 3
-prior_s = 3
-prior_r = 5
-
+#######################################################################################################################
 # The run_ABC_rejection function returns a data table with all simulation records
 # Input: 
 #       prior_params: prior distrinution parameters
@@ -25,26 +23,54 @@ prior_r = 5
 #       threshold: acceptane threshold
 # Output: 
 #       a data table 'sim' that contains all simulation records
-run_ABC_rejection = function(prior_mean, prior_sd, prior_s, prior_r, n, threshold) {
+
+run_ABC_rejection = function(prior_mean, prior_sd, prior_s, prior_r, iters, threshold, metric, summary) {
   #sample n pairs of parameters from prior distribution
-  tmp_mean <- rnorm(n, mean = prior_mean, sd = prior_sd) #改成class
-  tmp_precision <- rgamma(n, prior_s, prior_r)
+  tmp_mean <- rnorm(iters, mean = prior_mean, sd = prior_sd) #改成class
+  tmp_precision <- rgamma(iters, prior_s, prior_r)
   
-  sim_mean = rep(NA, n)
-  sim_precision = rep(NA, n)
-  sim_deviation = rep(NA, n)
-  sim_acceptance = rep(NA, n)
+  sim_mean = rep(NA, iters)
+  sim_precision = rep(NA, iters)
+  sim_deviation = rep(NA, iters)
+  sim_acceptance = rep(NA, iters)
   
-  for (i in 1:n){
+  for (i in 1:iters){
     # For each lambda, generate a simulated data with 13 cases
     samples = rnorm(5, tmp_mean[i], sqrt(1/tmp_precision[i]))
     sorted_samples <- sort(samples)
     sim_mean[i] = mean(samples)
     sim_precision[i] = 1/(sd(samples))^2
     
+    if ("data" %in% summary) {
+      data_stat <- data
+      sample_stat <- sorted_samples
+    } else {
+      data_stat <- c()
+      sample_stat <- c()
+      for (s in summary) {
+        if (s == "mean") {
+          data_stat <- c(data_stat, meandata)
+          sample_stat <- c(sample_stat, mean(sorted_samples))
+        } else if (s == "sd") {
+          data_stat <- c(data_stat, standarddeviationdata)
+          sample_stat <- c(sample_stat, sd(sorted_samples))
+        } else if (s == "median") {
+          data_stat <- c(data_stat, mediandata)
+          sample_stat <- c(sample_stat, median(sorted_samples))
+        } else if (s == "range") {
+          data_stat <- c(data_stat, rangedata)
+          sample_stat <- c(sample_stat, max(sorted_samples) - min(sorted_samples))
+        }
+      }
+    }
+    
     # compute the deviation by takeing the Euclidean distance between observed summary statistics
-    sim_deviation[i] = dist(rbind(dat,sorted_samples))
-    #sim_deviation[i] = sqrt(sum((mean(samples) - mean_data)^2 + (sd(samples) - sd_data)^2))
+    
+    if (metric == "L1Norm") {
+      sim_deviation[i] = dist(rbind(data_stat, sample_stat), method = "manhattan")
+    } else if (metric == "L2Norm") {
+      sim_deviation[i] = dist(rbind(data_stat, sample_stat))
+    }
     
     # Accept if deviation is smaller than threshhold; reject otherwise.
     if(sim_deviation[i] < threshold)
@@ -52,24 +78,22 @@ run_ABC_rejection = function(prior_mean, prior_sd, prior_s, prior_r, n, threshol
     else
       sim_acceptance[i] = FALSE
   }
-  sim = data.frame(index = c(1:reps), tmp_mean, tmp_precision, sim_mean, sim_precision, sim_deviation, sim_acceptance)
+  sim = data.frame(index = c(1:iters), tmp_mean, tmp_precision, sim_mean, sim_precision, sim_deviation, sim_acceptance)
   return(sim)
 }
 
-run_ABC <- function(input, threshold) {
-  if (input$method == "rejection") {
-    return(run_ABC_rejection(input$meanPrior, input$sdPrior, input$sPrior, input$rPrior,reps, threshold))
-  } else if (input$method == "m2") {
-    # TODO: replace with method 2
-    print("Method 2 selected")
-    return(run_ABC_rejection(input$meanPrior, input$sdPrior, input$sPrior, input$rPrior,reps, threshold))
-  } else if (input$method == "m3") {
-    # TODO: replace with method 3
-    print("Method 3 selected")
-    return(run_ABC_rejection(input$meanPrior, input$sdPrior, input$sPrior, input$rPrior,reps, threshold))
+#######################################################################################################################
+
+run_ABC <- function(input) {
+  if (input$method == "Rejection") {
+    return(run_ABC_rejection(input$meanPrior, input$sdPrior, input$sPrior, input$rPrior,input$iterations, input$threshold, input$metric, input$summary))
+  } else if (input$method == "ABC_MCMC") {
+    # TODO: replace with ABC MCMC
+  } else if (input$method == "ABC_SMC") {
+    # TODO: replace with ABC SMC
   }
 }
-
+#########################################################################################################################
 normal_plot <- function(mean, sd){
   gnorm <- ggplot(NULL,aes(x=c(mean - 4*sd,mean+4*sd))) +
     stat_function(fun=dnorm, args=list(mean=mean, sd=sd)) +
@@ -98,23 +122,37 @@ precision_trace_plot<-function(ourdata) {
   ggplot(ourdata,aes(x=index, y=sim_precision)) +
     geom_point(data=filter(ourdata, sim_acceptance==TRUE)) + 
     geom_line(data=filter(ourdata, sim_acceptance==TRUE)) + 
-    geom_point(data=filter(ourdata, sim_acceptance==FALSE), aes(x=index, y=sim_mean, color="red"))
+    geom_point(data=filter(ourdata, sim_acceptance==FALSE), aes(x=index, y=sim_precision, color="red"))
 }
 
+smc_mean_posterior_plot<-function(ourdata){
+  ggplot(ourdata,aes(x = saved_mean)) +
+    geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.5) 
+}
+
+smc_precision_posterior_plot<-function(ourdata){
+  ggplot(ourdata,aes(x = saved_precision)) +
+    geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.5) 
+}  
+
+#############################################################################################################3
 shinyServer(function(input, output) {
+
+  output$text2 <- renderUI({
+    HTML("my awesome text message in HTML!!!")
+  })
   
   output$priorPrecisionPdf <- renderPlot({
-    gamma_plot(s=input$sPrior, r=input$rPrior) + lims(y=c(0, 1))
+    gamma_plot(s=input$sPrior, r=input$rPrior) + lims(y=c(0, input$ymax2))
   })
   
   output$priorMeanPdf <- renderPlot({
-    normal_plot(mean=input$meanPrior, sd=input$sdPrior) + lims(y=c(0,input$ymax))
+    normal_plot(mean=input$meanPrior, sd=input$sdPrior) + lims(y=c(0,input$ymax1))
   })
   
   simulation = eventReactive(input$runButton, {
-    ABC_rejection_simulation = run_ABC(input, threshold)
+    ABC_rejection_simulation = run_ABC(input)
   })
-  
   
   output$traceMean <- renderPlot({
     mean_trace_plot(simulation())
@@ -127,15 +165,12 @@ shinyServer(function(input, output) {
   output$posteriorMeanPdf <- renderPlot({
     simulation() %>% filter(sim_acceptance == TRUE) %>%
       ggplot(aes(x = tmp_mean)) +
-      geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.5) #+
-    #geom_vline(xintercept = mean(ABC_rejection_posterior$tmp_mean),color="blue",linetype = "longdash")
-    
+      geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.5)
   })
   
   output$posteriorPrecisionPdf <- renderPlot({
     simulation() %>% filter(sim_acceptance == TRUE) %>%
       ggplot(aes(x = tmp_precision)) +
-      geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.1) #+
-    #geom_vline(xintercept = mean(ABC_rejection_posterior$tmp_precision),color="blue",linetype = "longdash")
+      geom_histogram(aes(y=..density..), color = 'white', boundary = 0, binwidth = 0.1)
   })
 })
--- ui.R
+++ ui.R
@@ -1,46 +1,114 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  
+  h2("Approximate Bayesian Computation on Normal-Normal"),
+  
+  hr(),
+  
+  h3("Section I: Normal-Normal model"),
+  
+  
+  p("Given a student’s scores on the first five Bayes homeworks, we can construct a normal-normal Bayesian model to make inferences about this student’s underlying Bayes skill level"), 
+  p("How to add Latex form equations????"),
+  hr(),
+  
+  h3("Section II: Tune Prior Parameters"),
+  
+  p("Recall that N(mu,sd) models the average bayes score of this student. Use the slider to update mu~N(85,9). This particular normal prior indicates that his score is likely between 74 and 94. Use the third slider to adjust y-aixs limit of the plot."),
+  
+  fluidRow(
+    column(4,
+           #h4("Tune the Normal(mu,sd) prior:"), 
+           sliderInput("meanPrior", "parameter mu", min = 0, max = 100, value = 5),
+           sliderInput("sdPrior", "parameter sd", min = 0, max = 100, value = 5),
+           sliderInput("ymax1", "Specify the limit of the y-axis:", min = 0, max = 1.5, value = 0.1)
+    ),
+    
+    column(6,
+           h4("Plot 1: Prior mean pdf:"), 
+           plotOutput("priorMeanPdf", width = "450px", height = "200px")
+    )
+  ),
+  
+  hr(),
+
+  p("The parameter tau models the precision of scores. Update it to Gamma(3, 5), which indicates that variance of scores is 0-10 from homework to homework. Use the third slider to adjust y-aixs limit of the plot."),
+  
+  fluidRow(
+    column(4,
+           # the Gamma prior
+           #h4("Tune the Gamma(s,r) prior:"), 
+           sliderInput("sPrior", "parameter s", min = 0, max = 100, value = 5),
+           sliderInput("rPrior", "parameter r", min = 0, max = 100, value = 5),
+           sliderInput("ymax2", "Specify the limit of the y-axis:", min = 0, max = 1.5, value = 0.1)
+    ),
+    
+    column(6,
+           h4("Plot 2: prior pdf for precision:"), 
+           plotOutput("priorPrecisionPdf", width = "450px", height = "200px")
+    )
+  ),
+  
+  hr(),
+  
+  h3("Section II: Set ABC parameters"),
+  p("The ABC algorithms dependend hevealy on parameters, such as the number of iterations, rejection threshold, and summary statistic methods. After setting these parameters, click run to produce trace plots and approximated posteriors."),
+  
   sidebarLayout(
     sidebarPanel(
       selectInput("method", "Specify ABC method:",
-                  c("Rejection" = "rejection",
-                    "Method2" = "m2",
-                    "Method3" = "m3")),
-
+                  c("Rejection" = "Rejection",
+                    "ABC_MCMC" = "ABC_MCMC",
+                    "ABC_SMC" = "ABC_SMC")),
+      
+      checkboxGroupInput("summary", "Choose the summary statistics",
+                         c("Mean" = "mean", 
+                           "Standard Deviation" = "sd",
+                           "Median" = "median",
+                           "Range" = "range",
+                           "Just data" = "data")),
+      
+      
+      selectInput("metric", "Choose the metric:",
+                  c("L-1 Norm" = "L1Norm",
+                    "L-2 Norm" = "L2Norm")),
+      
       # the Gamma prior
-      h4("Tune the Gamma(r,s) prior:"), 
-      sliderInput("rPrior", "parameter r", min = 0, max = 100, value = 5),
-      sliderInput("sPrior", "parameter s", min = 0, max = 100, value = 5),
+      #h4("Tune the Gamma(r,s) prior:"), 
+      #sliderInput("rPrior", "parameter r", min = 0, max = 100, value = 5),
+      #sliderInput("sPrior", "parameter s", min = 0, max = 100, value = 5),
       
       # the Normal prior 
-      h4("Tune the Normal(mu,sd) prior:"), 
-      sliderInput("meanPrior", "parameter mu", min = 0, max = 100, value = 5),
-      sliderInput("sdPrior", "parameter sd", min = 0, max = 100, value = 5),
+      #h4("Tune the Normal(mu,sd) prior:"), 
+      #sliderInput("meanPrior", "parameter mu", min = 0, max = 100, value = 5),
+      #sliderInput("sdPrior", "parameter sd", min = 0, max = 100, value = 5),
       
-      h4("Specify the limit of the y-axis:"), 
-      sliderInput("ymax", "", min = 0, max = 1, value = 0.1),
+  
+      #sliderInput("ymax", "Specify the limit of the y-axis:", min = 0, max = 1, value = 0.1),
       
-      #h4("Specify sample data:"), 
-      #sliderInput("n", "n", min = 1, max = 1000, value = 12)
+      sliderInput("iterations", "Specify number of iterations:", value = 5000, min = 1000, max = 10000, step= 100),
+      
+      sliderInput("threshold", "Specify threshhold:", min = 0, max = 100, value = 30, step = 5),
       
       actionButton("runButton", "Run Simulation")
     ),
     
     mainPanel(
-      h4("Plot of the prior pdf for mean:"), 
-      plotOutput("priorMeanPdf", width = "450px", height = "200px"),
-      h4("Plot of the prior pdf for precision:"), 
-      plotOutput("priorPrecisionPdf", width = "450px", height = "200px"),
+    
+      #h4("Plot of the prior pdf for mean:"), 
+      #plotOutput("priorMeanPdf", width = "450px", height = "200px"),
+      #h4("Plot of the prior pdf for precision:"), 
+      #plotOutput("priorPrecisionPdf", width = "450px", height = "200px"),
       
-      h4("Plot of the proposed mean"), 
+      h4("Plot 3: proposed mean"), 
       plotOutput("traceMean", width = "500px", height = "250px"),
-      h4("Plot of the proposed precision"), 
+      h4("Plot 4: proposed precision"), 
       plotOutput("tracePrecision", width = "500px", height = "250px"),
       
-      h4("Plot of the posterier mean"), 
+      h4("Plot 5: posterier mean"), 
       plotOutput("posteriorMeanPdf", width = "450px", height = "200px"),
-      h4("Plot of the posterier precision"), 
+      h4("Plot 6: posterier precision"), 
       plotOutput("posteriorPrecisionPdf", width = "450px", height = "200px")
     )
   )
